7d7a2c4d2cc257692544f09d3dd4c6bc5dfdbc2e
Matt Fannin <matt.fannin@powershop.co.nz>
2018-04-25 19:55:11 +1200

    Basic site now runs on Gatsby.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,16 @@
 import React from "react"
+import Helmet from "react-helmet"
 
-export default () => <div>Hello world!</div>
+import styles from "styles/main.scss"
+
+export default () =>
+  <section>
+    <Helmet
+      title="Matt Fannin | Designer & Photographer"
+      meta={[
+        { name: "theme-color", content: "#ffffff" }
+      ]}
+    />
+    <h1>Matt Fannin</h1>
+    <p>Designer & Photographer</p>
+  </section>
