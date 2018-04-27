e560c396cb81b4d8b6d91035d0b3b5d6ce7b689c
Matt Fannin <matt.fannin@powershop.co.nz>
2018-04-28 10:27:24 +1200

    Add google analytics.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,19 @@ module.exports = {
     `gatsby-plugin-resolve-src`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-37182164-16",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        // anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        // exclude: ["/preview/**", "/do-not-track/me/too/"],
+      },
+    },
   ]
 };
