e7241386b924f7cfd493137457b3bb175cc59362
Matt Fannin <matt.fannin@powershop.co.nz>
2018-07-11 00:40:29 +1200

    Seperates header from nav and add profile image

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Helmet from "react-helmet"
 
+import Header from "components/header"
 import Nav from "components/navigation"
 
 import styles from "styles/main.scss"
@@ -8,11 +9,12 @@ import styles from "styles/main.scss"
 export default ({ children }) => (
   <div className="container">
     <Helmet
-      title="Matt Fannin | Designer & Photographer"
+      title="Matt Fannin | Designer, photographer & creative thinker"
       meta={[
         { name: "theme-color", content: "#ffffff" }
       ]}
     />
+    <Header />
     <Nav />
     {children()}
   </div>
