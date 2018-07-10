e1feaf98f604ccbb2e7e48a57f2f28f364cb9adb
Matt Fannin <matt.fannin@powershop.co.nz>
2018-07-11 01:12:05 +1200

    Fixes image alignment issue

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <a href="#">
+        <a href="#" className="profile">
           <img src="http://www.gravatar.com/avatar/10bcecde76c1d8731d63687fd242d290.jpg?s=1200"></img>
         </a>
 
