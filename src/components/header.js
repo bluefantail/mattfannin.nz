cad541b7896e07ba8c0dbdcf27f8854cd231bb41
Matt Fannin <matt.fannin@powershop.co.nz>
2018-07-12 23:20:09 +1200

    Adds profile image detail view

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,23 @@ import Link from "gatsby-link"
 import Nav from "components/navigation"
 
 class Header extends React.Component {
+  componentDidMount() {
+    let profile = document.querySelector('.profile');
+    let profileDetail = document.querySelector('.profile-detail');
+    let closeProfile = document.querySelector('.close-profile');
+
+    profile.addEventListener('click', function() {
+      profileDetail.classList.toggle('visible');
+    });
+
+    closeProfile.addEventListener('click', function() {
+      profileDetail.classList.toggle('visible');
+    });
+  }
   render() {
     return (
       <header>
-        <a href="#" className="profile">
-          <img src="http://www.gravatar.com/avatar/10bcecde76c1d8731d63687fd242d290.jpg?s=280"></img>
-        </a>
+        <img className="profile" src="http://www.gravatar.com/avatar/10bcecde76c1d8731d63687fd242d290.jpg?s=280"></img>
 
         <div>
           <Link to="/">Matt Fannin</Link>
