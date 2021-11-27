ce3b51772ef495d9b1e386962de77a5768c5650b
Matt Fannin <hello@mattfannin.nz>
2021-11-27 19:21:35 +1300

    Add flickr link to website

--- src/components/offsite-links.js
+++ src/components/offsite-links.js
@@ -4,10 +4,11 @@ class OffsiteLinks extends React.Component {
   render() {
     return (
       <span className="offsite-links">
-        <a href="https://github.com/bluefantail" className="github" target="_blank" rel="noopener noreferrer"><span>Github</span></a>
-        <a href="https://codepen.io/bluefantail/" className="codepen" target="_blank" rel="noopener noreferrer"><span>Codepen</span></a>
-        <a href="https://medium.com/@mattfannin" className="medium" target="_blank" rel="noopener noreferrer"><span>Medium</span></a>
-        <a href="https://twitter.com/FanninMatt" className="twitter" target="_blank" rel="noopener noreferrer"><span>Twitter</span></a>
+        <a href="https://github.com/bluefantail" className="github" target="_blank" rel="noopener noreferrer" title="Github"><span>Github</span></a>
+        <a href="https://codepen.io/bluefantail/" className="codepen" target="_blank" rel="noopener noreferrer" title="Codepen"><span>Codepen</span></a>
+        <a href="https://medium.com/@mattfannin" className="medium" target="_blank" rel="noopener noreferrer" title="Medium"><span>Medium</span></a>
+        <a href="https://twitter.com/FanninMatt" className="twitter" target="_blank" rel="noopener noreferrer" title="Twitter"><span>Twitter</span></a>
+        <a href="https://www.flickr.com/photos/mfannin" className="flickr" target="_blank" rel="noopener noreferrer" title="Flickr"><span>Flickr</span></a>
       </span>
     )
   }
