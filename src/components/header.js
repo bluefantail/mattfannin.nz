a95f3edcf93c8fc6917034135fbc8819da592ac7
Matt Fannin <mattfannin@acidic.co.nz>
2024-05-14 00:39:53 +0000

    Change about info

--- src/components/header.js
+++ src/components/header.js
@@ -30,7 +30,7 @@ class Header extends React.Component {
 
         <div>
           <Link to="/">Matt Fannin</Link>
-          <div className="description">Designer &amp; thoughtful human</div>
+          <div className="description">Design · Code · Art · Thoughts</div>
         </div>
 
         <Nav />
